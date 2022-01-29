662edbe9bbc509348757202f075a8c8e55633d3c
NickMel25 <98445352+NickMel25@users.noreply.github.com>
2022-01-29 12:16:31 +0200

    initial commit

---
+++ config.py
@@ -0,0 +1,33 @@
+WIN_WIDTH = 640
+WIN_HEIGHT = 480
+TILESIZE = 32
+
+FPS = 60
+
+PLAYER_LAYER = 3
+BLOCK_LAYER = 2
+GROUND_LAYER = 1
+
+PLAYER_SPEED = 3
+
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+BLACK = (0, 0, 0)
+
+tilemap = [
+    'BBBBBBBBBBBBBBBBBBBB',
+    'B..................B',
+    'B..................B',
+    'B...BBBBB..........B',
+    'B..................B',
+    'B..................B',
+    'B...........BB.....B',
+    'B........P..B......B',
+    'B...........B......B',
+    'B..................B',
+    'B..................B',
+    'B..................B',
+    'B..................B',
+    'B..................B',
+    'BBBBBBBBBBBBBBBBBBBB',
+]
