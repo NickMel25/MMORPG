2758930531e09fab047ef3ac2c816012e38647a1
NickMel25 <98445352+NickMel25@users.noreply.github.com>
2022-01-30 09:47:04 +0200

    omg pogrond ur code is shit i fixed so much stuff but there is still a lot to do

--- game.py
+++ game.py
@@ -2,23 +2,25 @@ import pygame
 from config import *
 
 
-class Spritesheet:
-    def _init_(self, file):
-        self.sheet = pygame.image.load(file).convert
+class SpriteSheet:
+    def __init__(self, file):
+        super().__init__()
+        self.sheet = pygame.image.load(file)
+        self.sheet.set_colorkey((255, 255, 255))
+        self.sheet = self.sheet.convert_alpha()
 
     def get_sprite(self, x, y, width, height):
-        sprite = pygame.surface([width, height])
-        sprite.bilt(self.sheet, (0, 0), (x, y, width, height))
+        sprite = pygame.Surface([width, height])
+        sprite.blit(self.sheet, (0, 0), (x, y, width, height))
         sprite.set_colorkey(BLACK)
         return sprite
 
 
 class Player(pygame.sprite.Sprite):
-    def _init_(self, game, x, y):
+    def __init__(self, game, x, y):
+        super().__init__(game.all_sprites)
         self.game = game
         self._layer = PLAYER_LAYER  # tells pygame which layer the player will be in
-        self.groups = self.game.all_sprites
-        pygame.sprite.Sprite.init(self, self.groups)
         self.x = x * TILESIZE
         self.y = y * TILESIZE
 
@@ -30,63 +32,62 @@ class Player(pygame.sprite.Sprite):
         self.width = TILESIZE
         self.height = TILESIZE
 
-        self.image = self.game.character_spritesheet.get_Sprite(3, 2, self.width, self.height)
+        self.image = self.game.character_spritesheet.get_sprite(3, 2, self.width, self.height)
 
-        self.rect = self.get_rect()
+        self.rect = self.image.get_rect()
         self.rect.x = self.x
         self.rect.y = self.y
 
-        def update(self):
-            self.movement()
+    def update(self):
+        self.movement()
 
-            self.rect.x += self.x_change
-            self.collide_blocks('x')
-            self.rect.y += self.y_change
-            self.collide_blocks('y')
+        self.rect.x += self.x_change
+        self.collide_blocks('x')
+        self.rect.y += self.y_change
+        self.collide_blocks('y')
 
-            self.x_change = 0
-            self.y_change = 0
+        self.x_change = 0
+        self.y_change = 0
 
-        def movement(self):
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_LEFT]:
-                self.x_change -= PLAYER_SPEED
-                self.facing = 'left'
-            if keys[pygame.K_RIGHT]:
-                self.x_change += PLAYER_SPEED
-                self.facing = 'right'
-            if keys[pygame.K_UP]:
-                self.y_change -= PLAYER_SPEED
-                self.facing = 'up'
-            if keys[pygame.K_DOWN]:
-                self.y_change += PLAYER_SPEED
-                self.facing = 'down'
+    def movement(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.x_change -= PLAYER_SPEED
+            self.facing = 'left'
+            self.image = self.game.character_spritesheet.get_sprite(3, 4, self.width, self.height)
+        if keys[pygame.K_RIGHT]:
+            self.x_change += PLAYER_SPEED
+            self.facing = 'right'
+        if keys[pygame.K_UP]:
+            self.y_change -= PLAYER_SPEED
+            self.facing = 'up'
+        if keys[pygame.K_DOWN]:
+            self.y_change += PLAYER_SPEED
+            self.facing = 'down'
 
-        def collide_blocks(self, direction):
-            if direction == "x":
-                hits = pygame.sprite.Spritecollide(self, self.game.blocks, False)
-                if hits:
-                    if self.x_change > 0:
-                        self.rect.x = hits[0].rect.left - self.rect.width
-                    if self.x_change < 0:
-                        self.rect.x = hits[0].rect.right
+    def collide_blocks(self, direction):
+        if direction == "x":
+            hits = pygame.sprite.spritecollide(self, self.game.blocks, False)
+            if hits:
+                if self.x_change > 0:
+                    self.rect.x = hits[0].rect.left - self.rect.width
+                if self.x_change < 0:
+                    self.rect.x = hits[0].rect.right
 
-            if direction == "y":
-                hits = pygame.sprite.Spritecollide(self, self.game.blocks, False)
-                if hits:
-                    if self.y_change > 0:
-                        self.rect.y = hits[0].rect.top - self.rect.height
-                    if self.y_change < 0:
-                        self.rect.y = hits[0].rect.bottom
+        if direction == "y":
+            hits = pygame.sprite.spritecollide(self, self.game.blocks, False)
+            if hits:
+                if self.y_change > 0:
+                    self.rect.y = hits[0].rect.top - self.rect.height
+                if self.y_change < 0:
+                    self.rect.y = hits[0].rect.bottom
 
 
 class Block(pygame.sprite.Sprite):
-    def _init_(self, game, x, y):
+    def __init__(self, game, x, y):
+        super().__init__(game.all_sprites, game.blocks)
         self.game = game
         self._layer = BLOCK_LAYER
-        self.groups = self.game.all_sprites, self.game.blocks
-        pygame.sprite.Sprite.init(self, self.groups)
-
         self.x = x * TILESIZE
         self.y = y * TILESIZE
         self.width = TILESIZE
@@ -98,20 +99,20 @@ class Block(pygame.sprite.Sprite):
         self.rect.x = self.x
         self.rect.y = self.y
 
-        class Ground(pygame.sprite.Sprite):
-            def _init_(self, game, x, y):
-                self.game = game
-                self._layer = GROUND_LAYER
-                self.groups = self.game.all_sprites
-                pygame.sprite.Sprite.init(self, self.groups)
 
-                self.x = x * TILESIZE
-                self.y = y * TILESIZE
-                self.width = TILESIZE
-                self.height = TILESIZE
+class Ground(pygame.sprite.Sprite):
+    def __init__(self, game, x, y):
+        super().__init__(game.all_sprites)
+        self.game = game
+        self._layer = GROUND_LAYER
 
-                self.image = self.game.terrain_spritesheet.get_sprite(64, 352, self.width, self.height)
+        self.x = x * TILESIZE
+        self.y = y * TILESIZE
+        self.width = TILESIZE
+        self.height = TILESIZE
 
-                self.rect = self.image.get_rect()
-                self.rect.x = self.x
-                self.rect.y = self.y
+        self.image = self.game.terrain_spritesheet.get_sprite(64, 352, self.width, self.height)
+
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
--- main.py
+++ main.py
@@ -1,16 +1,74 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import pygame
+from config import *
+import sys
+from game import *
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class Game:
+    def __init__(self):
+        pygame.init()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+        self.clock = pygame.time.Clock()
+
+        self.running = True
+
+        self.character_spritesheet = SpriteSheet('res/character.png')
+        self.terrain_spritesheet = SpriteSheet('res/terrain.png')
+
+    def createTileMap(self):
+        for i, row in enumerate(tilemap):
+            for j, column in enumerate(row):
+                Ground(self, j, i)
+                if column == "B":
+                    Block(self, j, i)
+                if column == "P":
+                    Player(self, j, i)
+
+    def new(self):
+        # a new game starts
+        self.playing = True
+
+        self.all_sprites = pygame.sprite.LayeredUpdates()
+        self.blocks = pygame.sprite.LayeredUpdates()
+        self.enemies = pygame.sprite.LayeredUpdates()
+        self.attacks = pygame.sprite.LayeredUpdates()
+        self.createTileMap()
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.playing = False
+                self.running = False
+
+    def update(self):
+        self.all_sprites.update()
+
+    def draw(self):
+        self.screen.fill(BLACK)
+        self.all_sprites.draw(self.screen)
+        self.clock.tick(FPS)
+        pygame.display.update()
+
+    def main(self):
+        while self.playing:
+            self.events()
+            self.update()
+            self.draw()
+            self.running = False
+
+    def gameover(self):
+        pass
+
+    def intro_screen(self):
+        pass
+
+
+g = Game()
+g.intro_screen()
+g.new()
+while g.running:
+    g.main()
+    g.gameover()
+pygame.quit()
+sys.exit()
