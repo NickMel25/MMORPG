a816800f4be457a0dc06676bd766191d2d1a7a38
NickMel25 <98445352+NickMel25@users.noreply.github.com>
2022-02-17 19:14:55 +0200

    new game

--- main.py
+++ main.py
@@ -1,81 +1,33 @@
 import pygame
-from config import *
 import sys
-from game import *
+from settings import *
+from level import Level
 
 
 class Game:
     def __init__(self):
-        pygame.init()
 
-        self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+        # general setup
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGTH))
+        pygame.display.set_caption('MMO Game')
         self.clock = pygame.time.Clock()
 
-        self.running = True
+        self.level = Level()
 
-        self.character_spritesheet = SpriteSheet('res/character.png')
-        self.terrain_spritesheet = SpriteSheet('res/terrain.png')
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
 
-    def createTileMap(self):
-        for y, row in enumerate(tilemap):
-            for x, column in enumerate(row):
-                Ground(self, x, y)
-
-        for i, row in enumerate(tilemap):
-            for j, column in enumerate(row):
-                if column == "P":
-                    Player(self, j, i)
-                if column == "B":
-                    Block(self, j, i)
-                if column == "W":
-                    Water(self, j, i)
-
-    def new(self):
-        # a new game starts
-        self.playing = True
-
-        self.all_sprites = pygame.sprite.LayeredUpdates()
-        self.blocks = pygame.sprite.LayeredUpdates()
-        self.enemies = pygame.sprite.LayeredUpdates()
-        self.attacks = pygame.sprite.LayeredUpdates()
-        self.createTileMap()
+            self.screen.fill('black')
+            self.level.run()
+            pygame.display.update()
+            self.clock.tick(FPS)
 
 
-    def events(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.playing = False
-                self.running = False
-
-    def update(self):
-        self.all_sprites.update()
-
-    def draw(self):
-        self.screen.fill(BLACK)
-        self.all_sprites.draw(self.screen)
-
-        self.clock.tick(FPS)
-        pygame.display.update()
-
-    def main(self):
-        while self.playing:
-            self.events()
-            self.update()
-            self.draw()
-            self.running = False
-
-    def gameover(self):
-        pass
-
-    def intro_screen(self):
-        pass
-
-
-g = Game()
-g.intro_screen()
-g.new()
-while g.running:
-    g.main()
-    g.gameover()
-pygame.quit()
-sys.exit()
+if __name__ == '__main__':
+    game = Game()
+    game.run()
