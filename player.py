f06a7dba8e9a415b2ad529d7dd69160c916f8ee4
NickMel25 <98445352+NickMel25@users.noreply.github.com>
2022-02-20 08:22:47 +0200

    graphics and shit

--- player.py
+++ player.py
@@ -5,7 +5,7 @@ from settings import *
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups, obstacle_sprites):
         super().__init__(groups)
-        self.image = pygame.image.load('res/rat2.png').convert_alpha()  # find graphics
+        self.image = pygame.image.load('graphics/test/player.png').convert_alpha()  # find graphics
         self.rect = self.image.get_rect(topleft=pos)
         self.hitbox = self.rect.inflate(0, -26)
 
