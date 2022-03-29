dd325a0a06a5aabb8945ce5b7d6bdaf5f672cf77
NickMel25 <98445352+NickMel25@users.noreply.github.com>
2022-03-29 14:37:13 +0300

    bla

--- udp_client.py
+++ udp_client.py
@@ -14,9 +14,7 @@ udp_client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 udp_client.bind((ip,port))
 
 
-
-
-def recv_thread_handler(player,level):
+def recv_thread_handler(player, level):
     while True:
         data = receive()
         data = data
@@ -24,22 +22,24 @@ def recv_thread_handler(player,level):
         location = client_performer.display_players(data,player,level)
 
 
-
 def receive():
-        msg ,conn= udp_client.recvfrom(1024)
+        msg ,conn = udp_client.recvfrom(1024)
         msg = msg.decode()
         print(msg)
         return msg
+
 
 def send(msg):
     global server_IP
     global server_port
     udp_client.sendto(str.encode(msg),(server_IP,server_port))
 
+
 def proccess(data):    
     send(data)
     ans = receive()
     print(ans)
+
 
 def start_thread(player,level):
     # arr = {"player" : player,
